bec9f80283caae35173859c3e927c00264172dfd
junyian <junyian@gmail.com>
2026-03-29 10:28:19 +0800

    feat: add flake config

---
+++ flake.nix
@@ -0,0 +1,19 @@
+{
+  description = "Hugo blog development environment";
+
+  inputs = {
+    nixpkgs.url = "github:nixos/nixpkgs?ref=nixos-unstable";
+  };
+
+  outputs =
+    { self, nixpkgs }:
+    let
+      system = "x86_64-linux";
+      pkgs = nixpkgs.legacyPackages.${system};
+    in
+    {
+      devShells.${system}.default = pkgs.mkShell {
+        buildInputs = [ pkgs.hugo ];
+      };
+    };
+}
